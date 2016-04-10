4f8337aad5f75fd8d63000682892943aa31629f4
Aaron Kable <aaronkable@gmail.com>
2016-04-10 16:19:12 +0800

    inline build

--- Android.mk
+++ Android.mk
@@ -6,9 +6,23 @@ LOCAL_PACKAGE_NAME := BrokenSwitcher
 
 LOCAL_SRC_FILES := $(call all-java-files-under, app/src/main)
 
-
 LOCAL_STATIC_JAVA_LIBRARIES := \
+    android-support-v4 \
     android-support-v7-appcompat \
     android-support-design
 
-include $(BUILD_MULTI_PREBUILT)
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/app/src/main/res
+LOCAL_RESOURCE_DIR += $(TOP)/frameworks/support/v7/appcompat/res
+LOCAL_RESOURCE_DIR += $(TOP)/frameworks/support/design/res
+
+LOCAL_AAPT_FLAGS := \
+    --auto-add-overlay \
+    --extra-packages android.support.v7.appcompat \
+    --extra-packages android.support.design \
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+LOCAL_PRIVILEGED_MODULE := true
+
+include $(BUILD_PACKAGE)
+
