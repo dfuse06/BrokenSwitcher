bb667f869c99f31960cb6cb154dd6aa6c8f27611
dfuse06 <dfuse06@gmail.com>
2016-05-25 12:12:42 -0500

    add some settings, BTW arkansasdave is a bitch!

--- app/src/main/java/com/dfuse/brokenswitcher/MainActivity.java
+++ app/src/main/java/com/dfuse/brokenswitcher/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -122,5 +123,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item){
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
+
