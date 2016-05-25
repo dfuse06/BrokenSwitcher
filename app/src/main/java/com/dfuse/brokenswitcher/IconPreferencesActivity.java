b35041b8c253ffd3f383ad388d58a3c61c12613b
dfuse06 <dfuse06@gmail.com>
2016-05-25 17:41:17 -0500

    Fixed up a little bit

--- app/src/main/java/com/dfuse/brokenswitcher/IconPreferencesActivity.java
+++ app/src/main/java/com/dfuse/brokenswitcher/IconPreferencesActivity.java
@@ -25,20 +25,20 @@ public class IconPreferencesActivity extends PreferenceActivity {
                 addPreferencesFromResource(R.xml.drawer_icon);
 
                 PreferenceManager preferenceManager = getPreferenceManager();
-                if (preferenceManager.getSharedPreferences().getBoolean("drawer_icon", true)){
+                if (preferenceManager.getSharedPreferences().getBoolean("drawer_icon", true)) {
                     PackageManager p = getActivity().getPackageManager();
                     ComponentName componentName = new ComponentName(getActivity(), com.dfuse.brokenswitcher.LaunchIconActivity.class);
                     p.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
                 } else {
-                    PackageManager p = getActivity().getPackageManager();
-                    ComponentName componentName = new ComponentName(getActivity(), com.dfuse.brokenswitcher.LaunchIconActivity.class);
-                    p.setComponentEnabledSetting(componentName,PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
-
-
-                }
+                    int newState;
+                    newState = PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+                    PackageManager pm = getActivity().getPackageManager();
+                    pm.setComponentEnabledSetting(new ComponentName(getActivity(), LaunchIconActivity.class), newState, PackageManager.DONT_KILL_APP);
             }
 
 
+            }
         }
+
     }
 }
